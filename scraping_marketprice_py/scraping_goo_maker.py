bf6d26eaf68bacce4bc4beaf30bfee70b6a2764d
Shoki-Hiraga <chaser.cb750@gmail.com>
2025-01-12 04:47:15 +0900

    スクレイピングのファイルのダイエット

--- scraping_marketprice_py/scraping_goo_maker.py
+++ scraping_marketprice_py/scraping_goo_maker.py
@@ -6,26 +6,9 @@ import re
 from dotenv import load_dotenv
 import os
 
-# Laravelのルートディレクトリにある.envファイルのパスを指定
-LARAVEL_ENV_PATH = "/.env"  # 修正してください
-# .envファイルをロード
-load_dotenv(LARAVEL_ENV_PATH)
-# APP_URLの値を取得
-app_url = os.getenv("APP_URL")
-
-if app_url in ["http://localhost", "http://127.0.0.1/"]:
-    from setting_file.set import local
-    print("Local environment settings are active.")
-else:
-    from setting_file.set import production
-    print("Production environment settings are active.")
-
-DB_CONFIG = {
-    'host': os.getenv('DB_HOST'),
-    'user': os.getenv('DB_USERNAME'),
-    'password': os.getenv('DB_PASSWORD'),
-    'database': os.getenv('DB_DATABASE')
-}
+# Laravel環境とDB設定をロード
+from setting_script.setFunc import load_environment_and_get_config
+DB_CONFIG = load_environment_and_get_config()
 
 # 定義されたURLとセレクター
 website_url = "https://www.goo-net.com/"
