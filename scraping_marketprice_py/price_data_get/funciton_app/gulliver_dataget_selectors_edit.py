618e08893cd235e80b50e853d0805c22123a1a94
Shoki-Hiraga <chaser.cb750@gmail.com>
2025-01-26 01:39:46 +0900

    404エラーロジック追加、ガリバーグレードデータ加工追加

--- scraping_marketprice_py/price_data_get/funciton_app/gulliver_dataget_selectors_edit.py
+++ scraping_marketprice_py/price_data_get/funciton_app/gulliver_dataget_selectors_edit.py
@@ -1,8 +1,7 @@
 import re
 
 def process_data(selector, raw_data):
-    """
-    Process the raw data based on the selector.
+    """Process the raw data based on the selector.
     This function is designed to handle multiple scraping programs with different selectors.
     """
     processed_data = raw_data.strip()  # Trim whitespace
@@ -17,6 +16,11 @@ def process_data(selector, raw_data):
         match = re.search(r"^(.+?)\(", processed_data)
         if match:
             processed_data = match.group(1)
+    elif selector == "div.resut-carinfo--item:nth-of-type(n+2) div.carinfo-name":
+        # Extract the first half-width character and remove text before it
+        match = re.search(r".*?([ -~])", processed_data)  # [ -~] matches half-width characters
+        if match:
+            processed_data = processed_data[match.start(1):]  # Remove text before the first half-width character
     elif selector == "div.resut-carinfo--item:nth-of-type(n+2) div.carinfo-datepub":
         # Extract year in Western calendar (e.g., 2012)
         match = re.search(r"\d{4}", processed_data)
