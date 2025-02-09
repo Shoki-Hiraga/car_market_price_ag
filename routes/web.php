28cf8f9172f39333788a96c41ae0c336926a0ab6
Shoki-Hiraga <chaser.cb750@gmail.com>
2025-02-09 21:31:16 +0900

    sitemapを追加

--- routes/web.php
+++ routes/web.php
@@ -4,22 +4,22 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ScGooMakerController;
 use App\Http\Controllers\ScGooModelController;
 use App\Http\Controllers\ScGooGradeController;
-use App\Http\Controllers\MarketPriceMasterController;
+
+// sitemap用
+use Illuminate\Http\Response;
+use App\Models\ScGooMaker;
+use App\Models\ScGooModel;
+use App\Models\ScGooGrade;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| Webサイトのルート定義
 |
 */
 
-Route::get('/', function () {
-    return view('main.index');
-});
 // ホーム
 Route::get('/', [ScGooMakerController::class, 'index'])->name('maker.index');
 
@@ -29,5 +29,56 @@ Route::get('/model', [ScGooModelController::class, 'index'])->name('model.index'
 // モデル詳細
 Route::get('/model/{id}', [ScGooModelController::class, 'show'])->name('model.detail');
 
-// グレード詳細（モデルの下の階層にする）
+// グレード詳細
 Route::get('/model/{model_id}/grade/{grade_id}', [ScGooGradeController::class, 'show'])->name('grade.detail');
+
+
+// sitemap.xmlの動的生成
+Route::get('/sitemap.xml', function () {
+    // XMLの開始
+    $xml = '<?xml version="1.0" encoding="UTF-8"?>';
+    $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+
+    // ホームページ
+    $xml .= '<url>';
+    $xml .= '<loc>' . url(route('maker.index')) . '</loc>';
+    $xml .= '<changefreq>daily</changefreq>';
+    $xml .= '<priority>1.0</priority>';
+    $xml .= '</url>';
+
+    // モデル一覧ページ
+    $xml .= '<url>';
+    $xml .= '<loc>' . url(route('model.index')) . '</loc>';
+    $xml .= '<changefreq>weekly</changefreq>';
+    $xml .= '<priority>0.8</priority>';
+    $xml .= '</url>';
+
+    // モデル詳細ページ
+    $models = ScGooModel::latest()->get();
+    foreach ($models as $model) {
+        $xml .= '<url>';
+        $xml .= '<loc>' . url(route('model.detail', ['id' => $model->id])) . '</loc>';
+        $xml .= '<lastmod>' . $model->updated_at->toW3cString() . '</lastmod>';
+        $xml .= '<changefreq>weekly</changefreq>';
+        $xml .= '<priority>0.7</priority>';
+        $xml .= '</url>';
+    }
+
+    // グレード詳細ページ（model_id が null でない場合のみ）
+    $grades = ScGooGrade::latest()->get();
+    foreach ($grades as $grade) {
+        if (!is_null($grade->model_id)) { // model_id が null でないかチェック
+            $xml .= '<url>';
+            $xml .= '<loc>' . url(route('grade.detail', ['model_id' => $grade->model_id, 'grade_id' => $grade->id])) . '</loc>';
+            $xml .= '<lastmod>' . $grade->updated_at->toW3cString() . '</lastmod>';
+            $xml .= '<changefreq>monthly</changefreq>';
+            $xml .= '<priority>0.6</priority>';
+            $xml .= '</url>';
+        }
+    }
+
+    // XMLの閉じタグ
+    $xml .= '</urlset>';
+
+    return response($xml, 200)->header('Content-Type', 'application/xml');
+});
