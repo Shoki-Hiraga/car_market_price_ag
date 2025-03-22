504a7cdaa3502c3148f786a504f1ed1e2cd5353a
Shoki-Hiraga <chaser.cb750@gmail.com>
2025-03-23 00:39:29 +0900

    サイトマップ追加

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,15 @@ use App\Http\Controllers\ScGooMakerController;
 use App\Http\Controllers\ScGooModelController;
 use App\Http\Controllers\ScGooGradeController;
 use App\Http\Controllers\MpmMakerModelController;
+use App\Http\Controllers\ScGooMileageController;
+use App\Http\Controllers\ScGooYearController;
 
 // sitemap用
 use Illuminate\Http\Response;
 use App\Models\ScGooMaker;
 use App\Models\ScGooModel;
 use App\Models\ScGooGrade;
-use App\Http\Controllers\ScGooMileageController;
-use App\Http\Controllers\ScGooYearController;
+use App\Models\MarketPriceMaster;
 
 /*
 |--------------------------------------------------------------------------
@@ -46,8 +47,10 @@ Route::get('/sitemap.xml', function () {
     $xml = '<?xml version="1.0" encoding="UTF-8"?>';
     $xml .= '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
-    $xml .= '<sitemap><loc>' . url('/sitemap-models.xml') . '</loc></sitemap>';
-    $xml .= '<sitemap><loc>' . url('/sitemap-grades.xml') . '</loc></sitemap>';
+    $xml .= '<sitemap><loc>' . url('/sitemap-model.xml') . '</loc></sitemap>';
+    $xml .= '<sitemap><loc>' . url('/sitemap-grade.xml') . '</loc></sitemap>';
+    $xml .= '<sitemap><loc>' . url('/sitemap-mileage.xml') . '</loc></sitemap>';
+    $xml .= '<sitemap><loc>' . url('/sitemap-year.xml') . '</loc></sitemap>';
 
     $xml .= '</sitemapindex>';
 
@@ -94,6 +97,79 @@ Route::get('/sitemap-grade.xml', function () {
         $xml .= '<changefreq>monthly</changefreq>';
         $xml .= '<priority>0.6</priority>';
         $xml .= '</url>';
+    }
+
+    $xml .= '</urlset>';
+
+    return response($xml, 200)->header('Content-Type', 'application/xml');
+});
+
+// 走行距離専用サイトマップ
+Route::get('/sitemap-mileage.xml', function () {
+    $xml = '<?xml version="1.0" encoding="UTF-8"?>';
+    $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+
+    $grades = ScGooGrade::whereIn('id', function ($query) {
+        $query->select('grade_name_id')->from('market_price_master')->whereNotNull('mileage')->distinct();
+    })->latest()->limit(50000)->get();
+
+    foreach ($grades as $grade) {
+        $mileageCategories = MarketPriceMaster::where('model_name_id', $grade->model_name_id)
+            ->where('grade_name_id', $grade->id)
+            ->pluck('mileage')
+            ->map(function ($mileage) {
+                return floor($mileage); // 万km単位
+            })
+            ->unique()
+            ->sort();
+
+        foreach ($mileageCategories as $category) {
+            $url = route('mileage.detail', [
+                'model_id' => $grade->model_name_id,
+                'grade_id' => $grade->id,
+                'mileage_category' => $category
+            ]);
+            $xml .= '<url>';
+            $xml .= '<loc>' . url($url) . '</loc>';
+            $xml .= '<changefreq>monthly</changefreq>';
+            $xml .= '<priority>0.4</priority>';
+            $xml .= '</url>';
+        }
+    }
+
+    $xml .= '</urlset>';
+
+    return response($xml, 200)->header('Content-Type', 'application/xml');
+});
+
+// 年式専用サイトマップ
+Route::get('/sitemap-year.xml', function () {
+    $xml = '<?xml version="1.0" encoding="UTF-8"?>';
+    $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+
+    $grades = ScGooGrade::whereIn('id', function ($query) {
+        $query->select('grade_name_id')->from('market_price_master')->whereNotNull('year')->distinct();
+    })->latest()->limit(50000)->get();
+
+    foreach ($grades as $grade) {
+        $years = MarketPriceMaster::where('model_name_id', $grade->model_name_id)
+            ->where('grade_name_id', $grade->id)
+            ->pluck('year')
+            ->unique()
+            ->sort();
+
+        foreach ($years as $year) {
+            $url = route('year.detail', [
+                'model_id' => $grade->model_name_id,
+                'grade_id' => $grade->id,
+                'year' => $year
+            ]);
+            $xml .= '<url>';
+            $xml .= '<loc>' . url($url) . '</loc>';
+            $xml .= '<changefreq>monthly</changefreq>';
+            $xml .= '<priority>0.4</priority>';
+            $xml .= '</url>';
+        }
     }
 
     $xml .= '</urlset>';
