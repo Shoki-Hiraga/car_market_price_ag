6561dc7511cd0ea84d008bb7217169a43f550881
Shoki-Hiraga <chaser.cb750@gmail.com>
2025-05-30 07:42:14 +0900

    メーカー編集機能追加

--- WP_CMS/cms-carpri.332web.com/wp-content/plugins/custom-laravel-access/custom-laravel-access.php
+++ WP_CMS/cms-carpri.332web.com/wp-content/plugins/custom-laravel-access/custom-laravel-access.php
@@ -5,6 +5,7 @@ Plugin Name: Laravel DB Access
 
 require_once plugin_dir_path(__FILE__) . 'laravel-db-connection.php';
 require_once plugin_dir_path(__FILE__) . 'model_contents.php';
+require_once plugin_dir_path(__FILE__) . 'model_maker.php';
 
 add_action('wp_ajax_get_models_by_maker', 'get_models_by_maker');
 function get_models_by_maker() {
